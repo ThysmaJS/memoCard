5e19c9e18640afbebb3887c302e9129badf0b7d5
Thysma <mathys.gaillet@gmail.com>
2024-06-25 16:59:17 +0200

    Pinia + axios packages

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -6,8 +6,4 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-Route::get('/version', function () {
-    return response()->json(['Laravel' => app()->version()]);
-});
-
 require __DIR__.'/auth.php';
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,12 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import './assets/main.css'
+import './axios'
 
+const pinia = createPinia()
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
