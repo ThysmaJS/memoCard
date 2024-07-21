7f21fe0e7ec1420b75d7601b883dcc208d196e8a
Thysma <mathys.gaillet@gmail.com>
2024-07-21 16:11:56 +0200

    back button

--- Frontend/src/views/ThemeView.vue
+++ Frontend/src/views/ThemeView.vue
@@ -1,30 +1,54 @@
 <template>
   <div class="theme-page">
-    <h1 class="text-3xl font-bold mb-6">Thèmes pour la catégorie: {{ categoryName }}</h1>
+    <div class="relative mb-6">
+      <button @click="goBack" class="mb-2 flex items-center text-blue-500 hover:text-blue-700">
+        <svg
+          class="w-6 h-6 mr-2"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 19l-7-7 7-7"
+          ></path>
+        </svg>
+        Retour
+      </button>
+      <h1 class="text-3xl font-bold mt-2 ml-16">Thèmes pour la catégorie: {{ categoryName }}</h1>
+    </div>
     <Loader v-if="loading" />
-    <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      <div
-        v-for="(theme, index) in themes"
-        :key="theme.id"
-        :style="{ backgroundColor: colors[index % colors.length] }"
-        class="block rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow duration-300"
-      >
-        <h2 class="text-4xl font-bold mb-4">{{ theme.name }}</h2>
-        <p class="text-lg mb-2">Créé par: {{ theme.user.name }}</p>
-        <p class="text-lg mb-6">{{ theme.description }}</p>
-        <div class="flex flex-col space-y-2">
-          <router-link
-            :to="{ name: 'CardsView', params: { themeId: theme.id } }"
-            class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-          >
-            Voir les cartes
-          </router-link>
-          <router-link
-            :to="{ name: 'DailyReview', params: { themeId: theme.id } }"
-            class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded"
-          >
-            Commencer la Révision
-          </router-link>
+    <div v-else>
+      <div v-if="themes.length === 0" class="text-center text-lg text-gray-500">
+        Aucun thème actif pour le moment
+      </div>
+      <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div
+          v-for="(theme, index) in themes"
+          :key="theme.id"
+          :style="{ backgroundColor: colors[index % colors.length] }"
+          class="block rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow duration-300"
+        >
+          <h2 class="text-4xl font-bold mb-4">{{ theme.name }}</h2>
+          <p class="text-lg mb-2">Créé par: {{ theme.user.name }}</p>
+          <p class="text-lg mb-6">{{ theme.description }}</p>
+          <div class="flex flex-col space-y-2">
+            <router-link
+              :to="{ name: 'CardsView', params: { themeId: theme.id } }"
+              class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+            >
+              Voir les cartes
+            </router-link>
+            <router-link
+              :to="{ name: 'DailyReview', params: { themeId: theme.id } }"
+              class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded"
+            >
+              Commencer la Révision
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -66,6 +90,9 @@ export default {
       } finally {
         this.loading = false
       }
+    },
+    goBack() {
+      this.$router.go(-1)
     }
   }
 }
@@ -74,5 +101,8 @@ export default {
 <style scoped>
 .theme-page {
   padding: 2rem;
+}
+button.absolute {
+  position: absolute;
 }
 </style>
